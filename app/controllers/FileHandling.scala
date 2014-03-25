2d73a594c870ed4e48a6195678efd977ff141674
geralddejong <gerald@delving.eu>
2014-03-25 12:44:04 +0100

    File uploading using multi-part form data.

--- app/controllers/FileHandling.scala
+++ app/controllers/FileHandling.scala
@@ -5,10 +5,20 @@ import java.io.File
 import play.api.Logger
 
 object FileHandling extends Controller {
-  def upload = Action(parse.temporaryFile) {
+
+  def upload = Action(parse.multipartFormData) {
     request =>
-      Logger.info(request.toString())
-      request.body.moveTo(new File("/tmp/xml-ray/" + java.util.UUID.randomUUID()))
-      Ok("File uploaded")
+      request.body.file("file").map { file =>
+        val filename = file.filename
+        val contentType = file.contentType
+        Logger.info(s"upload $filename ($contentType)")
+        file.ref.moveTo(new File(s"/tmp/xml-ray/$filename"))
+        Ok("File uploaded")
+      }.getOrElse {
+        Redirect(routes.Application.index()).flashing(
+          "error" -> "Missing file"
+        )
+      }
   }
+
 }
