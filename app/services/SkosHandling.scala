fba46ea67b6c85a562f846956f519645d68a9ae1
geralddejong <gerald@delving.eu>
2014-06-18 08:12:53 +0200

    All SKOS tags interpreted.

--- app/services/SkosHandling.scala
+++ app/services/SkosHandling.scala
@@ -18,38 +18,87 @@ package services
 
 import scala.io.Source
 import scala.xml.pull._
+import scala.xml.{MetaData, NamespaceBinding}
 
 trait SkosHandling {
 
   object SkosVocabulary {
+
+    val XML = "http://www.w3.org/XML/1998/namespace"
+    val RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    val SKOS = "http://www.w3.org/2004/02/skos/core#"
+
     def apply(source: Source) = {
+
       val events = new XMLEventReader(source)
+
+      def attribute(uri: String, label: String, attributes: MetaData, scope: NamespaceBinding) = {
+        attributes.get(uri, scope, label) match {
+          case None => None
+          case Some(x) => Some(x.head.text)
+        }
+      }
+
+      def about(attributes: MetaData, scope: NamespaceBinding) = attribute(RDF, "about", attributes, scope)
+      def lang(attributes: MetaData, scope: NamespaceBinding) = attribute(XML, "lang", attributes, scope)
+      def resource(attributes: MetaData, scope: NamespaceBinding) = attribute(RDF, "resource", attributes, scope)
+
       while (events.hasNext) {
         events.next() match {
 
+          case EvElemStart("rdf", "RDF", attributes, scope) =>
+            println("Let's get started")
+
+          case EvElemStart("skos", "ConceptScheme", attributes, scope) =>
+            println(s"ConceptScheme about ${about(attributes, scope)}")
+
+          case EvElemStart("skos", "Concept", attributes, scope) =>
+            println(s"Concept about ${about(attributes, scope)}")
+
+          case EvElemStart("skos", "definition", attributes, scope) =>
+            println(s"Definition lang ${lang(attributes, scope)}")
+
+          case EvElemStart("skos", "prefLabel", attributes, scope) =>
+            println(s"Pref Label lang ${lang(attributes, scope)}")
+
+          case EvElemStart("skos", "altLabel", attributes, scope) =>
+            println(s"Alt Label lang ${lang(attributes, scope)}")
+
+          case EvElemStart("skos", "inScheme", attributes, scope) =>
+            println(s"In Scheme resource ${resource(attributes, scope)}")
+
+          case EvElemStart("skos", "broader", attributes, scope) =>
+            println(s"Broader resource ${resource(attributes, scope)}")
+
+          case EvElemStart("skos", "narrower", attributes, scope) =>
+            println(s"Narrower resource ${resource(attributes, scope)}")
+
+          case EvElemStart("skos", "topConceptOf", attributes, scope) =>
+            println(s"Top Concept resource ${resource(attributes, scope)}")
+
           case EvElemStart(pre, label, attributes, scope) =>
-            println(s"Start $label")
+            println(s"!!!START $label")
             attributes.foreach {
               attr =>
                 println(s"Attribute $attr")
             }
 
           case EvText(text) =>
-            val crunched = FileHandling.crunchWhitespace(text)
-            if (!crunched.isEmpty) println(s"Text '$crunched'")
+          //            val crunched = FileHandling.crunchWhitespace(text)
+          //            if (!crunched.isEmpty) println(s"Text '$crunched'")
 
           case EvEntityRef(entity) =>
-            println(s"Entity $entity")
+          //            println(s"Entity $entity")
 
           case EvElemEnd(pre, label) =>
-            println(s"End $label")
+          //            println(s"End $label")
 
           case EvComment(text) =>
-            println(s"Comment $text")
+          //            println(s"Comment $text")
           //            FileHandling.stupidParser(text, string => node.value(FileHandling.translateEntity(string)))
 
           case x =>
-            println("EVENT? " + x) // todo: record these in an error file for later
+            println("EVENT? " + x)
         }
       }
     }
