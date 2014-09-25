3f7bed5e2d0ac913984d1046d72c5de768e4ae94
geralddejong <gerald@delving.eu>
2014-09-25 10:25:41 +0200

    Acceptable files just have to end with .xml or .xml.gz now

--- app/assets/javascripts/dashboard/dashboard-controllers.js
+++ app/assets/javascripts/dashboard/dashboard-controllers.js
@@ -229,9 +229,8 @@ define(["angular"], function () {
             //$files: an array of files selected, each file has name, size, and type.  Take the first only.
             if ($files.length && !file.uploading) {
                 var onlyFile = $files[0];
-                var part = onlyFile.name.match(/(.+)__(.+).xml.gz/);
-                if (!part) {
-                    alert("Sorry, the file must be named SPEC__PREFIX.xml.gz");
+                if (!(onlyFile.name.endsWith('.xml.gz') || onlyFile.name.endsWith('.xml'))) {
+                    alert("Sorry, the file must end with '.xml.gz' or '.xml'");
                     return;
                 }
                 file.uploading = true;
