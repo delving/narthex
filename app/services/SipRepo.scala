f23bef12390356c2215f951097dddd590aa4d8a6
geralddejong <gerald@delving.eu>
2014-11-11 09:01:46 +0100

    Engine function now described by in the SipMapper trait, and SipFile can produce one for a prefix.

--- app/services/SipRepo.scala
+++ app/services/SipRepo.scala
@@ -18,8 +18,9 @@ package services
 import java.io._
 import java.util.zip.ZipFile
 
-import eu.delving.metadata.{Path, RecDef, RecDefTree, RecMapping}
-import services.SipRepo.SipFile
+import eu.delving.groovy.{GroovyCodeResource, MappingRunner, MetadataRecordFactory, XmlSerializer}
+import eu.delving.metadata._
+import record.PocketParser.Pocket
 
 import scala.collection.JavaConversions._
 import scala.io.Source
@@ -28,111 +29,129 @@ import scala.io.Source
  * @author Gerald de Jong <gerald@delving.eu>
  */
 
-object SipRepo {
-
-
-  case class SipMapping(prefix: String, version: String, recDefTree: RecDefTree, validationXSD: String, recMapping: RecMapping)
-
-  val PrefixVersion = "(.*)_(.*)".r
-
-  case class SipFile(file: File) {
-
-    lazy val zipFile = new ZipFile(file)
-
-    lazy val entries = zipFile.entries().map(entry => entry.getName -> entry).toMap
-
-    def readMap(fileName: String): Option[Map[String, String]] = {
-      entries.get(fileName).map { entry =>
-        val inputStream = zipFile.getInputStream(entry)
-        val lines = Source.fromInputStream(inputStream, "UTF-8").getLines()
-        lines.flatMap { line =>
-          val equals = line.indexOf("=")
-          if (equals < 0) None else Some(line.substring(0, equals).trim -> line.substring(equals + 1).trim)
-        }.toMap
-      }
-    }
-
-    lazy val facts = readMap("dataset_facts.txt")
-
-    def fact(name: String): Option[String] = facts.flatMap(_.get(name)).find(_.nonEmpty)
-
-    lazy val spec = fact("spec")
-    lazy val orgId = fact("orgId")
-    lazy val provider = fact("provider")
-    lazy val datProvider = fact("dataProvider")
-    lazy val country = fact("country")
-    lazy val language = fact("language")
-    lazy val rights = fact("rights")
-    lazy val schemaVersions = fact("schemaVersions")
-
-    lazy val hints = readMap("hints.txt")
-
-    def hint(name: String): Option[String] = hints.flatMap(_.get(name)).find(_.nonEmpty)
-
-    lazy val harvestUrl = hint("harvestUrl")
-    lazy val harvestSpec = hint("harvestSpec")
-    lazy val harvestPrefix = hint("harvestPrefix")
-    lazy val recordCount = hint("recordCount")
-    lazy val recordRootPath = hint("recordRootPath")
-    lazy val uniqueElementPath = hint("uniqueElementPath")
-
-    lazy val schemaVersionSeq: Option[Seq[String]] = schemaVersions.map(commas => commas.split(" *,").toSeq)
-
-    def file(fileName: String): String = {
-      entries.get(fileName).map { entry =>
-        val inputStream = zipFile.getInputStream(entry)
-        Source.fromInputStream(inputStream, "UTF-8").mkString
-      } getOrElse (throw new RuntimeException(s"Unable to read file $fileName from ${file.getAbsolutePath}"))
-    }
-
-    def recDefTree(fileName: String): RecDefTree = {
-      entries.get(fileName).map { entry =>
-        val inputStream = zipFile.getInputStream(entry)
-        val recDef = RecDef.read(inputStream)
-        RecDefTree.create(recDef)
-      } getOrElse (throw new RuntimeException(s"Unable to read rec def $fileName from ${file.getAbsolutePath}"))
-    }
-
-    def recMapping(fileName: String, recDefTree: RecDefTree): RecMapping = {
-      entries.get(fileName).map { entry =>
-        val inputStream = zipFile.getInputStream(entry)
-        val mapping = RecMapping.read(inputStream, recDefTree)
-        mapping.getNodeMappings.foreach { m =>
-          val inputPath = m.inputPath.toString
-          val oldPocket = "/input"
-          if (inputPath.startsWith(oldPocket)) {
-            m.inputPath = Path.create(inputPath.replace("/metadata", ""))
-          }
-//          println(s"transform this path: ${m.inputPath}")
-        }
-        mapping
-      } getOrElse (throw new RuntimeException(s"Unable to read rec def $fileName from ${file.getAbsolutePath}"))
-    }
-
-    lazy val sipMappings: Map[String, SipMapping] = schemaVersionSeq.map { sequence =>
-      sequence.map { schemaVersion =>
-        val PrefixVersion(prefix, version) = schemaVersion
-        val tree = recDefTree(s"${schemaVersion}_record-definition.xml")
-        prefix -> SipMapping(
-          prefix = prefix,
-          version = version,
-          recDefTree = tree,
-          validationXSD = "when you want to validate"/* file(s"${schemaVersion}_validation.xsd")*/,
-          recMapping = recMapping(s"mapping_$prefix.xml", tree)
-        )
-      }.toMap
-    }.getOrElse(Map.empty[String, SipMapping])
-
-  }
-
-}
-
 class SipRepo(home: File) {
   home.mkdir()
 
   def latestSIPFile: Option[SipFile] = {
     val zipFIles = home.listFiles().filter(_.getName.endsWith("zip"))
-    zipFIles.sortBy(_.getName).lastOption.map(SipFile)
+    zipFIles.sortBy(_.getName).lastOption.map(SipFile(_))
   }
 
+}
+
+object SipFile {
+
+  case class SipMapping(prefix: String, version: String, recDefTree: RecDefTree, validationXSD: String, recMapping: RecMapping)
+
+  trait SipMapper {
+    def map(pocket: Pocket): String
+  }
+
+  val PrefixVersion = "(.*)_(.*)".r
+
+  def apply(zipFile: File) = new SipFile(zipFile)
+
+}
+
+class SipFile(file: File) {
+  import services.SipFile._
+  lazy val zipFile = new ZipFile(file)
+
+  lazy val entries = zipFile.entries().map(entry => entry.getName -> entry).toMap
+
+  def readMap(fileName: String): Option[Map[String, String]] = {
+    entries.get(fileName).map { entry =>
+      val inputStream = zipFile.getInputStream(entry)
+      val lines = Source.fromInputStream(inputStream, "UTF-8").getLines()
+      lines.flatMap { line =>
+        val equals = line.indexOf("=")
+        if (equals < 0) None else Some(line.substring(0, equals).trim -> line.substring(equals + 1).trim)
+      }.toMap
+    }
+  }
+
+  lazy val facts = readMap("dataset_facts.txt")
+
+  def fact(name: String): Option[String] = facts.flatMap(_.get(name))
+
+  lazy val spec = fact("spec")
+  lazy val orgId = fact("orgId")
+  lazy val provider = fact("provider")
+  lazy val datProvider = fact("dataProvider")
+  lazy val country = fact("country")
+  lazy val language = fact("language")
+  lazy val rights = fact("rights")
+  lazy val schemaVersions = fact("schemaVersions")
+
+  lazy val hints = readMap("hints.txt")
+
+  def hint(name: String): Option[String] = hints.flatMap(_.get(name))
+
+  lazy val harvestUrl = hint("harvestUrl")
+  lazy val harvestSpec = hint("harvestSpec")
+  lazy val harvestPrefix = hint("harvestPrefix")
+  lazy val recordCount = hint("recordCount")
+  lazy val recordRootPath = hint("recordRootPath")
+  lazy val uniqueElementPath = hint("uniqueElementPath")
+
+  lazy val schemaVersionSeq: Option[Seq[String]] = schemaVersions.map(commas => commas.split(" *,").toSeq)
+
+  def file(fileName: String): String = {
+    entries.get(fileName).map { entry =>
+      val inputStream = zipFile.getInputStream(entry)
+      Source.fromInputStream(inputStream, "UTF-8").mkString
+    } getOrElse (throw new RuntimeException(s"Unable to read file $fileName from ${file.getAbsolutePath}"))
+  }
+
+  def recDefTree(fileName: String): RecDefTree = {
+    entries.get(fileName).map { entry =>
+      val inputStream = zipFile.getInputStream(entry)
+      val recDef = RecDef.read(inputStream)
+      RecDefTree.create(recDef)
+    } getOrElse (throw new RuntimeException(s"Unable to read rec def $fileName from ${file.getAbsolutePath}"))
+  }
+
+  def recMapping(fileName: String, recDefTree: RecDefTree): RecMapping = {
+    entries.get(fileName).map { entry =>
+      val inputStream = zipFile.getInputStream(entry)
+      val mapping = RecMapping.read(inputStream, recDefTree)
+      mapping.getNodeMappings.foreach { nodeMapping =>
+        val inputPath = nodeMapping.inputPath.toString
+        if (inputPath.startsWith("/input")) {
+          nodeMapping.inputPath = Path.create(inputPath.replace("/metadata", ""))
+        }
+      }
+      mapping
+    } getOrElse (throw new RuntimeException(s"Unable to read rec def $fileName from ${file.getAbsolutePath}"))
+  }
+
+  lazy val sipMappings: Map[String, SipMapping] = schemaVersionSeq.map { sequence =>
+    sequence.map { schemaVersion =>
+      val PrefixVersion(prefix, version) = schemaVersion
+      val tree = recDefTree(s"${schemaVersion}_record-definition.xml")
+      prefix -> SipMapping(
+        prefix = prefix,
+        version = version,
+        recDefTree = tree,
+        validationXSD = "when you want to validate"/* file(s"${schemaVersion}_validation.xsd")*/,
+        recMapping = recMapping(s"mapping_$prefix.xml", tree)
+      )
+    }.toMap
+  }.getOrElse(Map.empty[String, SipMapping])
+
+  class MappingEngine(sipMapping: SipMapping) extends SipMapper {
+    val groovy = new GroovyCodeResource(getClass.getClassLoader)
+    val serializer = new XmlSerializer
+    val runner = new MappingRunner(groovy, sipMapping.recMapping, null, false)
+    val namespaces = sipMapping.recDefTree.getRecDef.namespaces.map(ns => ns.prefix -> ns.uri).toMap
+    val factory = new MetadataRecordFactory(namespaces)
+
+    override def map(pocket: Pocket): String = {
+      val metadataRecord = factory.metadataRecordFrom(pocket.id, pocket.text, false)
+      val result = new MappingResultImpl(serializer, pocket.id, runner.runMapping(metadataRecord), runner.getRecDefTree).resolve
+      result.toXmlAugmented
+    }
+  }
+
+  def createSipMapper(prefix:String): Option[SipMapper] = sipMappings.get(prefix).map(new MappingEngine(_))
 }
--- test/specs/TestSipRepo.scala
+++ test/specs/TestSipRepo.scala
@@ -2,15 +2,10 @@ package specs
 
 import java.io.File
 
-import eu.delving.groovy._
-import eu.delving.metadata.MappingResultImpl
 import harvest.HarvestRepo
 import harvest.Harvesting.HarvestType
 import org.scalatest.{FlatSpec, Matchers}
-import record.PocketParser.Pocket
 import services.{ProgressReporter, SipRepo}
-
-import scala.collection.JavaConversions._
 
 class TestSipRepo extends FlatSpec with Matchers {
 
@@ -33,39 +28,9 @@ class TestSipRepo extends FlatSpec with Matchers {
 
       sipFile.schemaVersionSeq.size should be(1)
 
-      sipFile.sipMappings.get("tib").map { tib =>
-
-        val mappingRunner: MappingRunner = {
-          val groovyCodeResource = new GroovyCodeResource(getClass.getClassLoader)
-          new MappingRunner(groovyCodeResource, tib.recMapping, null, false)
-        }
-
-        val namespaces: Map[String, String] = tib.recDefTree.getRecDef.namespaces.map(ns => ns.prefix -> ns.uri).toMap
-
-        val metadataRecordFactory = new MetadataRecordFactory(namespaces)
-
-        val serializer = new XmlSerializer
-
-        var caught = false
-
-        def pocketCatcher(pocket: Pocket) = {
-          if (!caught) {
-//            println(pocket)
-            val metadataRecord = metadataRecordFactory.metadataRecordFrom(pocket.id, pocket.text.replaceAll("pocket", "input"))
-            val child = metadataRecord.getRootNode.children().get(0).asInstanceOf[GroovyNode]
-            val mr = MetadataRecord.create(child, -1, -1)
-//            println(XmlNodePrinter.toXml(mr.getRootNode))
-            val result = new MappingResultImpl(serializer, pocket.id, mappingRunner.runMapping(mr), mappingRunner.getRecDefTree).resolve
-            println(result)
-          }
-          caught=true
-        }
-
-        harvestRepo.parsePockets(pocketCatcher, ProgressReporter())
-        true
-      }.getOrElse(throw new RuntimeException)
+      sipFile.createSipMapper("tib") map { sipMapper =>
+        harvestRepo.parsePockets(pocket => println(sipMapper.map(pocket)), ProgressReporter())
+      }
     }
   }
-
-
 }
