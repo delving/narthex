b2194f4ed46321e296f4867d719e78fc2bd6cbbb
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2024-11-06 22:21:01 +0100

    chore: clean up PreviewController

--- app/web/PreviewController.scala
+++ app/web/PreviewController.scala
@@ -10,42 +10,26 @@ class PreviewController (orgContext: OrgContext)(implicit val ec: ExecutionConte
 
   def previewHarvestUrl(dataUrl: String): Action[AnyContent] = Action.async { request =>
 
-    // Extract the incoming query parameters
     val queryParams: Map[String, Seq[String]] = request.queryString
 
-    // Convert the query parameters to the format expected in a URL string
     val queryString = queryParams.map { case (key, values) => 
       values.map(value => s"$key=$value").mkString("&")
     }.mkString("&")
 
-    // Append the query string to the URL
     val url = if (queryString.nonEmpty) {
       s"$dataUrl?$queryString"
     } else {
       s"$dataUrl"
     }
 
-    // Make a request to the external URL
     orgContext.wsApi.url(url).get().flatMap { response =>
-      // Extract headers from the response
       val headers = response.allHeaders.map { case (k, v) => k -> v.mkString(",") }
 
-      // Remove "Content-Length" from the headers
-      val filteredHeaders = headers.filterNot(_._1.equalsIgnoreCase("Content-Length"))
+      val filteredHeaders = headers.filterNot { case (key, _) =>
+        key.equalsIgnoreCase("Content-Length") || key.equalsIgnoreCase("Content-Type")
+      }
 
-      // Get the Content-Type header (if it exists)
-      val contentTypeHeader = headers.get("Content-Type")
-      
-      // Build the response
       val result = Ok(response.body)
-
-      // Apply Content-Type if present, otherwise return without setting it explicitly
-      // val finalResult = contentTypeHeader match {
-      //   case Some(contentType) => result.as(contentType)
-      //   case None => result
-      // }
-
-      // Return the result with headers, excluding Content-Length
       Future.successful(result.withHeaders(filteredHeaders.toSeq: _*))
     }.recover {
       // Handle errors, like if the URL is incorrect or there's a network issue
