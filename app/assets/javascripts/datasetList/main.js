2241e382eaa7a30170372d1c97f2dae7e3573196
Sjoerd Siebinga <sjoerd@delving.eu>
2025-12-04 11:47:50 +0100

    chore: bump version to 0.8.2.17

--- app/assets/javascripts/datasetList/main.js
+++ app/assets/javascripts/datasetList/main.js
@@ -15,7 +15,7 @@ define(
                 function ($routeProvider) {
                     $routeProvider.when(
                         "/", {
-                            templateUrl: "/narthex/assets/templates/dataset-list.html?v=0.8.2.16",
+                            templateUrl: "/narthex/assets/templates/dataset-list.html?v=0.8.2.17",
                             controller: controllers.DatasetListCtrl,
                             reloadOnSearch: false
                         }
