43526bae10ec46dbb8094f34d5ba1be25ef2f503
Sjoerd Siebinga <sjoerd@delving.eu>
2026-03-17 10:59:06 +0100

    chore: bump version to 0.8.7.16

--- app/assets/javascripts/datasetList/main.js
+++ app/assets/javascripts/datasetList/main.js
@@ -15,13 +15,13 @@ define(
                 function ($routeProvider) {
                     $routeProvider.when(
                         "/", {
-                            templateUrl: "/narthex/assets/templates/dataset-list.html?v=0.8.7.15",
+                            templateUrl: "/narthex/assets/templates/dataset-list.html?v=0.8.7.16",
                             controller: controllers.DatasetListCtrl,
                             reloadOnSearch: false
                         }
                     ).when(
                         "/sip-creator", {
-                            templateUrl: "/narthex/assets/templates/sip-creator-downloads.html?v=0.8.7.15",
+                            templateUrl: "/narthex/assets/templates/sip-creator-downloads.html?v=0.8.7.16",
                             controller: 'SipCreatorDownloadsCtrl'
                         }
                     );
